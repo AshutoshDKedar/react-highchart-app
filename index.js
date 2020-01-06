8851c0c356903600652f1534b9cda1aaf0b04be1
AshutoshDKedar <ashutoshdkedar@gmail.com>
2020-01-06 10:22:19 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 /*import '../node_modules/jquery/dist/jquery.min';
 import '../node_modules/popper.js/dist/popper.min';
 import '../node_modules/bootstrap/dist/js/bootstrap.min';
