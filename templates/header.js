68eed8749457e463846f0bdc91fadcaf100ee0c5
AshutoshDKedar <ashutoshdkedar@gmail.com>
2020-01-06 10:12:37 +0530

    3 files added

---
+++ templates/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+    render(){
+        return(
+            <header className="cmp-header row">
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    
+                </div>
+            </header>
+        );
+    }
+}
+
+export default Header;
