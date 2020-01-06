d6c99376ddc29e02bb627d6d5967874b3592e296
AshutoshDKedar <ashutoshdkedar@gmail.com>
2020-01-06 10:13:13 +0530

    1 file modified

---
+++ templates/footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component{
+    render(){
+        return(
+            <footer className="cmp-footer row">
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    
+                </div>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
