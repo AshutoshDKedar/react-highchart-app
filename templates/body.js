2fe59920cbbb553ede45ad68510cfb8562eb7cc4
AshutoshDKedar <ashutoshdkedar@gmail.com>
2020-01-06 10:12:55 +0530

    1 file modified

---
+++ templates/body.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import App from '../components/Advisor_Calculator/app';
+
+class Body extends Component{
+    render(){
+        return(
+            <div className="cmp-body">
+                <App />
+            </div>
+        );
+    }
+}
+
+export default Body;
